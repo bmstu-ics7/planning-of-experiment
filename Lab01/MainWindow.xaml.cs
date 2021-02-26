62a10568572c80ead5b0f5f5ed68e4454eb89b09
Willsem <happymainer@gmail.com>
2021-02-26 14:51:45 +0300

    :pencil: add lab01 design

---
+++ Lab01/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainWindowView();
+            Oxyplot_Test.InvalidatePlot(true);
+        }
+
+        private void Button_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowView)this.DataContext).Title = "Click";
+        }
+
+        private void ChechIsNumber(TextBox textBox)
+        {
+            string text = textBox.Text;
+
+            try
+            {
+                Convert.ToDouble(text);
+            }
+            catch (FormatException)
+            {
+                textBox.Text = text.Remove(text.Length - 1, 1);
+            }
+        }
+
+        private void TextBox_SigmaGenerator_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // ChechIsNumber(TextBox_SigmaGenerator);
+        }
+
+        private void TextBox_SigmaTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // ChechIsNumber(TextBox_SigmaTime);
+        }
+    }
+}
---
+++ Lab01/MainWindowView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using OxyPlot;
+
+namespace Lab01
+{
+    public class MainWindowView
+    {
+        public MainWindowView()
+        {
+            this.Title = "Test";
+            this.Points = new List<DataPoint>();
+
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                Points.Add(new DataPoint(x, x * x));
+            }
+        }
+
+        public string Title { get; set; }
+
+        public IList<DataPoint> Points { get; set; }
+    }
+}
